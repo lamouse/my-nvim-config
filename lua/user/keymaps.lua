a511088f1062aae478a631b6bcbf05cca3c3a2a5
wxc <softsora@163.com>
2026-04-21 23:23:11 +0800

    添加code action

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,3 +3,4 @@ local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 vim.keymap.set('n', "<leader>s", ":Outline<CR>", opts)
 vim.keymap.set('n', "<leader>t", ":Neotree<CR>", opts)
+vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = 'LSP Code Action' })
